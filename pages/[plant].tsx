6a203ef82e464588fefb8888fd054259e12bb3ba
Elias Peteri <elias.peteri@hotmail.com>
2022-07-07 13:46:12 +0300

    Created a dynamic route for single plant pages.

---
+++ pages/[plant].tsx
@@ -0,0 +1,39 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import data from '../data/plants.json';
+import { Plant } from '../types';
+
+const PlantPage = () => {
+  const [plant, setPlant] = useState<Plant>();
+  const router = useRouter();
+
+  useEffect(() => {
+    const plantName = router.query.plant as string;
+    const plant = data.find((item) => item.id === plantName);
+    setPlant(plant);
+  }, [router.query.plant]);
+  return (
+    <div>
+      <Head>
+        <title>Plantery {plant && `| ${plant.name}`}</title>
+      </Head>
+      <div>
+        <Link href='/'>
+          <a>Home</a>
+        </Link>
+        {plant && (
+          <div>
+            <h2>
+              {plant.name}, (<i>{plant.scientificName}</i>)
+            </h2>
+            <p>{plant.createdAt?.toLocaleString()}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default PlantPage;
