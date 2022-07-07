2834a71a686172d083e36770732abc5acc07db49
Elias Peteri <elias.peteri@hotmail.com>
2022-07-07 12:36:03 +0300

    Forgot to add search bar to the page in previous commit.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import Searchbar from '../components/Searchbar';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
@@ -12,7 +13,9 @@ const Home: NextPage = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <Searchbar />
+      </main>
 
       <footer className={styles.footer}>
         <a
