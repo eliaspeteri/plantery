503509937ce506b52cd2204dc3ab003ffa5f441d
Elias Peteri <elias.peteri@hotmail.com>
2022-08-31 09:26:24 +0300

    API should return 404 if data not found.

--- pages/api/plants.ts/index.ts
+++ pages/api/plants.ts/index.ts
@@ -3,6 +3,8 @@ import data from '../../../data/plants.json';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
-    res.status(200).json(data);
+    data
+      ? res.status(200).json(data)
+      : res.status(404).json({ message: 'data not found.' });
   }
 }
