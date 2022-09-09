5c0de706bb6399cf6a714ea1856a07c1ae0dce62
Elias Peteri <elias.peteri@hotmail.com>
2022-09-09 16:23:47 +0300

    Category page for forum.

--- pages/forum/categories/[category].tsx
+++ pages/forum/categories/[category].tsx
@@ -1,7 +1,27 @@
+import { InferGetServerSidePropsType } from 'next';
+import { useRouter } from 'next/router';
 import React from 'react';
+import { getCategory } from '../../api/categories/[category]';
 
-const CategoryPage = () => {
-  return <div>{}</div>;
+const CategoryPage = ({
+  category
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const router = useRouter();
+  return <div>{category?.title}</div>;
 };
 
 export default CategoryPage;
+
+export async function getServerSideProps({ req, res, query, params }) {
+  const category = await getCategory(params.category);
+  console.log(category);
+  if (category === undefined || category?.length === 0)
+    return {
+      notFound: true
+    };
+  return {
+    props: {
+      category
+    }
+  };
+}
