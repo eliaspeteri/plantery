1000a4314181ae079d959b23a513ac3a753f4dd2
Elias Peteri <elias.peteri@hotmail.com>
2022-09-21 09:31:39 +0300

    Searchbar handler function now uses useCallback-hook.

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import styles from '../styles/Searchbar.module.css';
 import { Plant } from '../types';
 import SearchResult from './SearchResult';
@@ -9,19 +9,22 @@ const Searchbar = () => {
   const [results, setResults] = useState<Plant[]>([]);
   const [search, setSearch] = useState('');
 
-  const onSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-    // If search matches data item, add to results
-    const matches = data.filter((item) => {
-      const regex = new RegExp(e.target.value, 'gi');
-      return item.name.match(regex) || item.latin.match(regex);
-    });
-    setResults(matches);
-    // If search is empty, reset results
-    if (e.target.value === '') {
-      setResults([]);
-    }
-  };
+  const onSearchChange = useCallback(
+    async (e: React.ChangeEvent<HTMLInputElement>) => {
+      setSearch(e.target.value);
+      // If search matches data item, add to results
+      const matches = data.filter((item) => {
+        const regex = new RegExp(search, 'gi');
+        return item.name.match(regex) || item.latin.match(regex);
+      });
+      setResults(matches);
+      // If search is empty, reset results
+      if (e.target.value === '') {
+        setResults([]);
+      }
+    },
+    [search]
+  );
 
   return (
     <div>
@@ -32,7 +35,7 @@ const Searchbar = () => {
         value={search}
         onChange={onSearchChange}
       />
-      {results.map((result) => (
+      {results?.map((result) => (
         <SearchResult result={result} key={result.id} />
       ))}
     </div>
